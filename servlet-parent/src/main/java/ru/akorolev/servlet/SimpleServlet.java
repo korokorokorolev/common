a8ed3210f3a335800c1e9ff3ef537d536c61d09c
akorolev <korokorokorolev@gmail.com>
2014-06-12 17:52:42 +0400

    add simple servlet implementation

---
+++ servlet-parent/src/main/java/ru/akorolev/servlet/SimpleServlet.java
@@ -0,0 +1,26 @@
+package ru.akorolev.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by akorolev on 6/12/14.
+ */
+@WebServlet("/123")
+public class SimpleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
